523cebcd8ed6876d06c8f1c290a99d92d803dbf7
Darío Vargas <dariovm13@gmail.com>
2021-09-20 15:55:35 -0600

    [FIX]:  Se crea la matriz de datos correctamente

--- Programa/Includes/File.hs
+++ Programa/Includes/File.hs
@@ -1,27 +1,43 @@
 module File where
+
 import System.IO
 
+-- Separa un string si es igual a un token
+-- Ejemplo:
+-- >> wordsWhen (==',') "hola,mundo" -> ["hola","mundo"]
 wordsWhen :: (Char -> Bool) -> [Char] -> [[Char]]
-wordsWhen p s =  case dropWhile p s of
-                      "" -> []
-                      s' -> w : wordsWhen p s''
-                            where (w, s'') = break p s'
+wordsWhen p s = case dropWhile p s of
+  "" -> []
+  s' -> w : wordsWhen p s''
+    where
+      (w, s'') = break p s'
 
-dataMatrixAux :: Monad m => [[Char]] -> [[Char]] -> m [[Char]]
-dataMatrixAux list newList =
-    if null list
-         then return  newList
-    else
-       dataMatrixAux (init list) (wordsWhen (==',') (last list)++newList)
+-- Función auxiliar que crea una matriz separando de los strings por coma
+-- Recibe:
+--  list: una lista de strings
+--  newList: una lista vacía
+-- Retorna:
+--  Una matriz de strings
+dataMatrixAux :: [[Char]] -> [[[Char]]] -> [[[Char]]]
+dataMatrixAux list newList = do
+  if null list
+    then newList
+    else dataMatrixAux (tail list) newList ++ [wordsWhen (== ',') (head list)]
 
-dataMatrix :: Monad m => [[Char]] -> m [[String]]
-dataMatrix list =
-    if null list
-        then return [[""]]
-    else
-        return (dataMatrixAux list [])
+-- Convierte una lista de strings y las convierte en una matriz
+-- Recibe :
+--  list: Una lista de tipo [[char]]
+-- Retorna:
+--  Una matris de tipo [[[char]]]
+dataMatrix :: [[Char]] -> [[[Char]]]
+dataMatrix list = do
+  if null list
+    then [[]]
+    else dataMatrixAux list []
 
+-- Crea una lista de las lineas de un archivo
+-- path: un string con la dirección del archivo
 getLines :: FilePath -> IO [String]
-getLines path  = do
-    str <- readFile  path
-    return (lines str)
+getLines path = do
+  str <- readFile path
+  return (lines str)
